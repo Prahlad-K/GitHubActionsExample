919a5721bfd15308221059d11aac8bbae77965e0
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-11-02 11:57:53 -0400

    Updated the Test file for Workflow to run correctly

--- helloTest.cpp
+++ helloTest.cpp
@@ -10,7 +10,7 @@ class Test : public CPPUNIT_NS::TestCase
 {
 CPPUNIT_TEST_SUITE(Test);
 CPPUNIT_TEST(testHelloWorld);
-CPPUNIT_TEST(failHelloWorld);
+// CPPUNIT_TEST(failHelloWorld);
 CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -23,10 +23,10 @@ protected:
         system("./hello >nul 2>nul");
     }
 
-    void failHelloWorld(void)
-    {
-        exit(1);
-    }
+    // void failHelloWorld(void)
+    // {
+    //     exit(1);
+    // }
 };
 
 
